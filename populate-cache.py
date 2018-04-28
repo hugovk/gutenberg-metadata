6cac3b8c58aec0f75b70339602976137fac93f88
Hugo <hugovk@users.noreply.github.com>
2018-04-28 13:34:55 +0300

    Add script to populate the gutenberg cache

---
+++ populate-cache.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Populate the gutenberg cache
+From https://github.com/c-w/gutenberg#looking-up-meta-data
+"""
+from gutenberg.acquire import get_metadata_cache
+print("Warning: this is slow...")
+cache = get_metadata_cache()
+cache.populate()
